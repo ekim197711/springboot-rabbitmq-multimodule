021563234e2f5bfcecb42d89358cdf1e338ac12d
Mike Møller Nielsen <mmni@protonmail.com>
2020-08-23 03:13:25 +0700

    multiple messages

--- rmq-consumer/src/main/java/com/example/demo/reject/PeekRestController.java
+++ rmq-consumer/src/main/java/com/example/demo/reject/PeekRestController.java
@@ -1,18 +1,14 @@
 package com.example.demo.reject;
 
-import com.example.demo.reject.RejectAndReadMessage;
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.GetResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.Message;
-import org.springframework.amqp.rabbit.core.ChannelCallback;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.rabbit.support.DefaultMessagePropertiesConverter;
-import org.springframework.amqp.rabbit.support.MessagePropertiesConverter;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,16 +17,16 @@ public class PeekRestController {
     private final RabbitTemplate rabbitTemplate;
 
     @GetMapping("/api/peek/{nMessages}")
-    public String peek(@PathVariable Integer nMessages){
-        String result ="Message from the queue: \n";
-        for (int i = 0; i < nMessages; i++){
-            Message message = rabbitTemplate.execute(new RejectAndReadMessage());
-            if (message == null)
-                break;
-            String body = new String(message.getBody());
-            result+=body + "\n";
+    public String peek(@PathVariable Integer nMessages) {
+        String result = "Message from the queue: \n";
+
+        List<Message> messages = rabbitTemplate.execute(new RejectAndReadMessage(nMessages));
+        for (Message message1 : messages) {
+            String body = new String(message1.getBody());
+            result += body + "\n";
             log.info("Messagebody peek: {}", body);
         }
+
         return result;
     }
 
--- rmq-consumer/src/main/java/com/example/demo/reject/RejectAndReadMessage.java
+++ rmq-consumer/src/main/java/com/example/demo/reject/RejectAndReadMessage.java
@@ -7,17 +7,33 @@ import org.springframework.amqp.rabbit.core.ChannelCallback;
 import org.springframework.amqp.rabbit.support.DefaultMessagePropertiesConverter;
 import org.springframework.amqp.rabbit.support.MessagePropertiesConverter;
 
-public class RejectAndReadMessage implements ChannelCallback<Message> {
+import java.util.ArrayList;
+import java.util.List;
+
+public class RejectAndReadMessage implements ChannelCallback<List<Message>> {
+    private final int nMessages;
     final MessagePropertiesConverter propertiesConverter = new DefaultMessagePropertiesConverter();
 
+    public RejectAndReadMessage(int nMessages) {
+        this.nMessages = nMessages;
+    }
+
     @Override
-    public Message doInRabbit(Channel channel) throws Exception {
-        GetResponse messageResponse = channel.basicGet("mike", false);
-        if (messageResponse == null) {
-            return null;
+    public List<Message> doInRabbit(Channel channel) throws Exception {
+        List<GetResponse> messages = new ArrayList<>();
+        List<Message> result = new ArrayList<>();
+        for (int i = 0; i < nMessages; i++){
+            GetResponse messageResponse = channel.basicGet("mike", false);
+            if (messageResponse == null) {
+                break;
+            }
+            messages.add(messageResponse);
         }
-        channel.basicReject(messageResponse.getEnvelope().getDeliveryTag(), true);
-        return new Message(messageResponse.getBody(), propertiesConverter.toMessageProperties(
-                messageResponse.getProps(), messageResponse.getEnvelope(), "UTF-8"));
+        for (GetResponse messageResponse : messages) {
+            channel.basicReject(messageResponse.getEnvelope().getDeliveryTag(), true);
+            result.add(new Message(messageResponse.getBody(), propertiesConverter.toMessageProperties(
+                    messageResponse.getProps(), messageResponse.getEnvelope(), "UTF-8")));
+        }
+        return result;
     }
 }
